dec393b2743e49448e122fb8949189fe6ab660cf
16thomja <jaredcthomas@hotmail.com>
2026-03-18 10:16:00 -0400

    Add latent space to video

--- elastic_pendulum_z_true_video.py
+++ elastic_pendulum_z_true_video.py
@@ -14,7 +14,7 @@ import os
 from functools import partial
 import subprocess
 
-def process_frame(x_frame, x_recon_frame, z, timestep, frames_folder):
+def process_frame(x_frame, z, timestep, frames_folder):
     z_plot = z[:timestep+1]
     z_plot_alpha = [0.2]*timestep + [1.0]
     z1_min = np.min(z[:,0])
@@ -22,18 +22,17 @@ def process_frame(x_frame, x_recon_frame, z, timestep, frames_folder):
     z2_min = np.min(z[:,1])
     z2_max = np.max(z[:,1])
 
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))  # 1 row, 3 columns
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))  # 1 row, 2 columns
 
     axes[0].imshow(x_frame.reshape((51, 51)), cmap='gray', origin='lower', vmin=0, vmax=1)
     axes[0].axis('off')
     axes[0].set_title("x")
     axes[1].scatter(z_plot[:,0], z_plot[:,1], alpha=z_plot_alpha)
-    axes[1].set_title("z")
+    axes[1].set_title("z_true")
+    axes[1].set_xlabel("r")
+    axes[1].set_ylabel("theta")
     axes[1].set_xlim(z1_min, z1_max)
     axes[1].set_ylim(z2_min, z2_max)
-    axes[2].imshow(x_recon_frame.reshape((51, 51)), cmap='gray', origin='lower', vmin=0, vmax=1)
-    axes[2].axis('off')
-    axes[2].set_title("x_recon")
 
     # Save the figure
     fig.savefig(frames_folder + f't{timestep}.png', bbox_inches="tight")
@@ -44,33 +43,19 @@ def main():
 
     cp_path, cp_folder = get_checkpoint_path(args)
     
-    frames_folder = cp_folder + "/frames/"
+    frames_folder = cp_folder + "/frames_z_true/"
 
     if not os.path.isdir(frames_folder):
         os.system("mkdir -p " + frames_folder)
 
-    _, _, test_set = load_data(args)
+    _, data_paths = get_data_paths()
 
-    if torch.cuda.is_available():
-        device = torch.device(f"cuda:{args.device}")
-        torch.cuda.set_device(device)
-    else:
-        device = torch.device("cpu")
-
-    net = make_model(args).to(device)
-    net, _, _, _ = load_model(net, cp_path, device)
+    test_set = np.load(data_paths[2], allow_pickle=True).item()
 
     sample_index = 1
 
-    x = test_set.x[sample_index].view(-1, net.u_dim).type(torch.FloatTensor).to(device)
-
-    with torch.no_grad():
-        z = net.encoder(x)
-        x_recon = net.decoder(z)
-
-    x = x.detach().cpu().numpy()
-    z = z.detach().cpu().numpy()
-    x_recon = x_recon.detach().cpu().numpy()
+    x = test_set['x'].reshape(-1, args.timesteps, args.u_dim)[sample_index]
+    z = test_set['z'].reshape(-1, args.timesteps, args.z_dim)[sample_index]
 
     with ThreadPoolExecutor(max_workers=32) as executor:
         process_frame_with_folder = partial(process_frame, frames_folder=frames_folder)
@@ -78,14 +63,13 @@ def main():
         list(tqdm(executor.map(
             process_frame_with_folder, 
             x,
-            x_recon,
             np.tile(z, (args.timesteps, 1, 1)),
             range(args.timesteps)
         ), total=args.timesteps))
 
     subprocess.call([
         'ffmpeg', '-y', '-framerate', '60', '-i', frames_folder + 't%d.png', '-r', '60', '-pix_fmt', 'yuv420p', '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2',
-        cp_folder + 'sample_autoencoder_output.mp4'
+        cp_folder + 'sample_z_true.mp4'
     ])
 
 if __name__ == "__main__":
